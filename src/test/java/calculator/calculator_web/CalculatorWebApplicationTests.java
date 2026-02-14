0836de8facb281feab9bd31e7a173d85bf4af5e0
Styagi01 <shaileshtyagi01@gmail.com>
2026-02-14 10:25:03 +0530

    Initial commit - Spring Boot Calculator

---
+++ src/test/java/calculator/calculator_web/CalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.calculator_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
